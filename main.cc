f59e5e87f25c7bf90059f0acfe48f9ad0aedec70
Kevin Wang <k433wang@uwaterloo.ca>
2022-11-28 22:58:52 -0500

    basic command interpreting skeleton

--- main.cc
+++ main.cc
@@ -1,7 +1,78 @@
 #include <iostream>
+#include <sstream>
 using namespace std;
 
 int main() {
-    cout << "Hello World!" << endl;
+    string input;
+
+    while (!cin.eof()) {
+        getline(cin, input);
+        istringstream ss{ input };
+        
+        if (input.size() == 0) {
+            continue;
+        } else {
+            string command;
+            ss >> command;
+            if (command == "game") {
+                string white;
+                string black;
+
+                ss >> white >> black;
+                if (white == "human") {
+                    
+                } else {
+
+                }
+
+                if (black == "human") {
+
+                } else {
+
+                }
+            }
+            if (command == "resign") {
+                // need a field in player to determine turn and decide
+            }
+            if (command == "move") {
+                string start;
+                string end;
+
+                ss >> start >> end;
+            }
+            if (command == "setup") {
+                while (!cin.eof()) {
+                    string setupInput, action;
+                    getline(cin, setupInput);
+
+                    istringstream ssSetup{ setupInput };
+                    ssSetup >> action;
+
+                    if (action == "+") {
+                        string piece, location;
+                        ssSetup >> piece >> location;
+
+                    }
+                    if (action == "-") {
+                        string location;
+                        ssSetup >> location;
+                        
+                    }
+                    if (action == "=") {
+                        string colour;
+                        ssSetup >> colour;
+                        if (colour == "B") {
+
+                        }
+                        if (colour == "W") {
+
+                        }
+                    }   
+                    if (action == "done") break;
+                }              
+            }
+        }
+    }
+
     return 0;
 }
