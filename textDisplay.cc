d7f0f6e6fb1c50baf07babfba7974d7af82751ba
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2022-11-30 15:42:16 -0500

    implement textDisplay

---
+++ textDisplay.cc
@@ -0,0 +1,24 @@
+#include <iostream>
+#include "textDisplay.h"
+
+using namespace std;
+
+void TextDisplay::notify() {
+  std::vector<std::vector<char>> pieces = board->getBoard();
+  for (int i = 0; i < 8; i++) {
+    cout << i + 1 << ' ';
+    for (int j = 0; j < 8; j++) {
+      if (pieces[i][j] != ' ') {
+        cout << pieces[i][j];
+      } else {
+        if (i % 2 == j % 2) {
+          cout << '_';
+        } else {
+          cout << ' ';
+        }
+      }
+    }
+    cout << '\n';
+  }
+  cout << "  abcdefgh" << endl;
+}
