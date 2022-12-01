9fd51a671b2f56962e710a9db873144fd4e9e171
Ibrahim Ahmed <ibrahim@ibrahimahmed.ca>
2022-12-01 13:16:18 -0500

    fix baseCheckValidity

--- player.cc
+++ player.cc
@@ -13,19 +13,22 @@ bool Player::baseCheckValidity(Move move) {
     if (!(dstX > 0 && dstX < boardArray.front().size() && dstY > 0 && dstY < boardArray.size())) 
         return false;
 
-    char piece = boardArray.at(dstY).at(dstX);
+    char originPiece = boardArray.at(originY).at(originX);
+    char dstPiece = boardArray.at(dstY).at(dstX);
     int vertical = abs(originY - dstY);
     int horizontal = abs(originX - dstX);
 
-    if (white && isupper(piece)) return false;
-    if (!white && islower(piece)) return false;
+    if (white && isupper(dstPiece)) return false;
+    if (white && islower(originPiece)) return false;
+    if (!white && islower(dstPiece)) return false;
+    if (!white && isupper(originPiece)) return false;
 
-    if ((piece == 'P' || 'p') && !((horizontal == 1 && vertical == 0) || (horizontal == 0 && vertical == 1))) return false;
-    if ((piece == 'K' || 'k') && !(horizontal == 1 || vertical == 1)) return false;
-    if ((piece == 'Q' || 'q') && !(horizontal == vertical || horizontal == 0 || vertical == 0)) return false;
-    if ((piece == 'R' || 'r') && !(horizontal == 0 || vertical == 0)) return false;
-    if ((piece == 'B' || 'b') && !(horizontal == vertical)) return false;
-    if ((piece == 'H' || 'h') && !((horizontal == 2 && vertical == 1) || (horizontal == 1 && vertical == 2))) return false;
+    if ((originPiece == 'P' || 'p') && !((horizontal == 1 && vertical == 0) || (horizontal == 0 && vertical == 1))) return false;
+    if ((originPiece == 'K' || 'k') && !(horizontal == 1 || vertical == 1)) return false;
+    if ((originPiece == 'Q' || 'q') && !(horizontal == vertical || horizontal == 0 || vertical == 0)) return false;
+    if ((originPiece == 'R' || 'r') && !(horizontal == 0 || vertical == 0)) return false;
+    if ((originPiece == 'B' || 'b') && !(horizontal == vertical)) return false;
+    if ((originPiece == 'H' || 'h') && !((horizontal == 2 && vertical == 1) || (horizontal == 1 && vertical == 2))) return false;
 
     return true;
 }
